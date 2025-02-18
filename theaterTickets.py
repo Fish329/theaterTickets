04b8430f293eb1d1d908c67affbab9df3c998ffc
Fish329 <268188@burlcoschools.org>
2025-02-18 10:55:02 -0500

    Add files via upload
    final (that was quick)

--- theaterTickets.py
+++ theaterTickets.py
@@ -10,27 +10,31 @@ seating=[ #prices of all 46 seats.
     [10, 10, 10, 10, 10, 10, 10, 10]]
 
 while True: #main loop
-    itr=0
-    nxt=7
-    for i in seating:
-        pass
-    #unfinished. TODO: print an aesthetically pleasing seating chart
     print("")
-    if seating==[[0]*8]*9: #if all seats are taken, exit
+    itr=0
+    for i in range(len(seating)):
+        print("[$",seating[i][0],"]","[$",seating[i][1],"]","[$",seating[i][2],"]","[$",seating[i][3],"]","[$",seating[i][4],"]","[$",seating[i][5],"]","[$",seating[i][6],"]","[$",seating[i][7],"]",sep="")
+    if seating==[["00"]*8]*9: #if all seats are taken, exit
         print("Sorry, we're booked.")
+        print("Have a nice day!")
         exit()
     seatCol=(input("Welcome to the theater! Please enter the column of the seat you want. (0-7) "))
-    if seatCol=="sold": #cheat
-        seating=[[0]*8]*9
+    if seatCol=="sold": #shortcut to sell all seats
+        seating=[["00"]*8]*9
         print ("all seats are now sold out.")
         continue
-    seatCol=int(seatCol)
-    seatRow=int(input("Now, input the row of the seat you want. (0-8) "))
-    if seatCol>7 or seatCol<0 or seatRow>8 or seatRow<0:
+    seatCol=int(seatCol) #shortcut is a string, so we need to convert to an integer after we check for it.
+    seatRow=int(input("Now, input the row of the seat you want. (0-8) ")) #for the collumn, since we aren't checking for a shortcut anymore, we can set the variable to an integer immediately.
+    if seatCol>7 or seatCol<0 or seatRow>8 or seatRow<0: #detect bad seat coordinates
         print("Sorry, we don't have that many seats.")
         continue
-    if seating[seatCol][seatRow]==0:
+    if seating[seatRow][seatCol]=="00": #detect taken seat
         print("Sorry, that seat's taken.")
         continue
+    print("Okay! That'll be ","$",seating[seatRow][seatCol],".",sep="") #update seating chart
+    seating[seatRow][seatCol]="00"
+    print("Next in line, please!")
+    continue
+
 
     
