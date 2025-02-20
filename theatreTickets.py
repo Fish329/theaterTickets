65b44f793591e60e1df4f931c2d271a3c34445e5
Fish329 <268188@burlcoschools.org>
2025-02-20 09:40:53 -0500

    Add files via upload
    reintroduced dollar symbols

--- theatreTickets.py
+++ theatreTickets.py
@@ -13,7 +13,7 @@ while True: #main loop
     print("")
     itr=0
     for i in range(len(seating)):
-        print(" ",seating[i][0]," "," ",seating[i][1]," "," ",seating[i][2]," "," ",seating[i][3]," "," ",seating[i][4]," "," ",seating[i][5]," "," ",seating[i][6]," "," ",seating[i][7]," ",sep="")
+        print("$",seating[i][0]," ","$",seating[i][1]," ","$",seating[i][2]," ","$",seating[i][3]," ","$",seating[i][4]," ","$",seating[i][5]," ","$",seating[i][6]," ","$",seating[i][7]," ",sep="")
     if seating==[["X "]*8]*9: #if all seats are taken, exit
         print("Sorry, we're booked.")
         print("Have a nice day!")
